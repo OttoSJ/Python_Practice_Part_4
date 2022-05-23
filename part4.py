de07ee1764190cc3a7ba942cadf07a3aa9c6797b
Otto Jones <ottosjonesjr@gmail.com>
2022-05-23 22:09:29 +0300

    first push

---
+++ part4.py
@@ -0,0 +1,38 @@
+#
+
+num = [5, 9, 2]
+
+# Answer to question # 1
+
+
+def max_num(num1, num2, num3):
+    return max(num1, num2, num3)
+
+
+# print(max_num(1, 6, 3))
+
+
+# Answer to question number # 2
+def my_list(list):
+    for num in list:
+        print(num * 5)
+
+
+# my_list(num)
+
+
+# Answer to question # 3
+string = 'Hello World'
+
+
+def rev_string(string):
+    return string[::-1]
+
+
+print(rev_string(string))
+
+# Answer to question # 4
+# I didn't understand this question
+
+# Answer to question # 5
+# I didn't understand this question
